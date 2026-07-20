dd538d4677c4512b4c1e3ace6a601971be58aef3
Lumpix09 <lumpix@streber24.de>
2026-07-20 21:09:19 +0200

    copy code from uniform motion to accelerated motion to have a base to start on

--- Lumpix.Movement.Calculator/Pages/accelerated_motion.xaml.cs
+++ Lumpix.Movement.Calculator/Pages/accelerated_motion.xaml.cs
@@ -13,18 +13,64 @@ using System.Windows.Shapes;
 
 namespace Lumpix.Movement.Calculator
 {
-    /// <summary>
-    /// Interaktionslogik für accelerated_motion.xaml
-    /// </summary>
     public partial class accelerated_motion : Page
     {
         public accelerated_motion()
         {
             InitializeComponent();
         }
+        #region Event Handlers
+        #region navigation
         private void btn_home_click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new MainPage());
         }
+        #endregion
+        #region calculations
+        #region calculation of final position
+        private void dusp_values_1_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            double d0 = dusp_start_pos_1.Value ?? 0;
+            double v = dusp_velocity_1.Value ?? 0;
+            double t = dusp_time_1.Value ?? 0;
+            double t0 = dusp_start_time_1.Value ?? 0;
+
+            double d = Math.Round(d0 + v * (t - t0), 3);
+
+            lbl_final_position_1.Content = d.ToString();
+        }
+
+        private void dusp_values_2_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            double d0 = dusp_start_pos_2.Value ?? 0;
+            double v = dusp_velocity_2.Value ?? 0;
+            double t = dusp_time_2.Value ?? 0;
+            double t0 = dusp_start_time_2.Value ?? 0;
+
+            double d = Math.Round(d0 + v * (t - t0), 3);
+
+            lbl_final_position_2.Content = d.ToString();
+        }
+        #endregion
+        #region calculation of intersection
+        private void btn_calc_intersection(object sender, RoutedEventArgs e)
+        {
+            double t1 = dusp_start_time_1.Value ?? 0;
+            double v1 = dusp_velocity_1.Value ?? 0;
+            double x1 = dusp_start_pos_1.Value ?? 0;
+            double t2 = dusp_start_time_2.Value ?? 0;
+            double v2 = dusp_velocity_2.Value ?? 0;
+            double x2 = dusp_start_pos_2.Value ?? 0;
+            double t01 = dusp_start_time_1.Value ?? 0;
+
+            double t_intersection = (-t2 * v2 + x2 + v1 * t1 - x1) / (v1 - v2);
+            lbl_out_time_of_intersection.Content = Math.Round(t_intersection, 3).ToString();
+
+            double d = Math.Round(x1 + v1 * (t_intersection - t01), 3);
+            lbl_out_place_of_intersection.Content = d.ToString();
+        }
+        #endregion
+        #endregion
+        #endregion
     }
 }
