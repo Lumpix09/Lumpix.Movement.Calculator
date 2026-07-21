96ae53f7fb02a1790953a91015aac2570cba12f3
Lumpix09 <lumpix@streber24.de>
2026-07-21 20:29:10 +0200

    added intersection calculation

--- Lumpix.Movement.Calculator/Pages/accelerated_motion.xaml.cs
+++ Lumpix.Movement.Calculator/Pages/accelerated_motion.xaml.cs
@@ -57,20 +57,23 @@ namespace Lumpix.Movement.Calculator
         #region calculation of intersection
         private void btn_calc_intersection(object sender, RoutedEventArgs e)
         {
-        // a formula to calculate where two accelerated motions meet is needed  
             double t1 = dusp_start_time_1.Value ?? 0;
             double v1 = dusp_velocity_1.Value ?? 0;
             double x1 = dusp_start_pos_1.Value ?? 0;
+            double a1 = dusp_acceleration_1.Value ?? 0;
             double t2 = dusp_start_time_2.Value ?? 0;
             double v2 = dusp_velocity_2.Value ?? 0;
             double x2 = dusp_start_pos_2.Value ?? 0;
             double t01 = dusp_start_time_1.Value ?? 0;
+            double a2 = dusp_acceleration_2.Value ?? 0;
 
-            double t_intersection = (-t2 * v2 + x2 + v1 * t1 - x1) / (v1 - v2);
-            lbl_out_time_of_intersection.Content = Math.Round(t_intersection, 3).ToString();
+            double t_solution1 = (-(v1 - a1 * t1 - v2 + a2 * t2) + Math.Sqrt(Math.Pow(v1 - a1 * t1 - v2 + a2 * t2, 2) - 2 * (a1 - a2) * (x1 - v1 * t1 + 0.5 * a1 * t1 * t1 - x2 + v2 * t2 - 0.5 * a2 * t2 * t2))) / (a1 - a2);
+            double t_solution2 = (-(v1 - a1 * t1 - v2 + a2 * t2) - Math.Sqrt(Math.Pow(v1 - a1 * t1 - v2 + a2 * t2, 2) - 2 * (a1 - a2) * (x1 - v1 * t1 + 0.5 * a1 * t1 * t1 - x2 + v2 * t2 - 0.5 * a2 * t2 * t2))) / (a1 - a2);
+            lbl_out_time_of_intersection.Content = Math.Round(t_solution1, 3).ToString() + " or " + Math.Round(t_solution2, 3).ToString();
 
-            double d = Math.Round(x1 + v1 * (t_intersection - t01), 3);
-            lbl_out_place_of_intersection.Content = d.ToString();
+            double d1 = Math.Round(x1 + v1 * (t_solution1 - t01) + 0.5 * a1 * Math.Pow(t_solution1 - t01, 2), 3);
+            double d2 = Math.Round(x1 + v1 * (t_solution2 - t01) + 0.5 * a1 * Math.Pow(t_solution2 - t01, 2), 3);
+            lbl_out_place_of_intersection.Content = d1.ToString() + " or " + d2.ToString();
         }
         #endregion
         #endregion
